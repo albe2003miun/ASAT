d0741aad39eff46f4de0d962f29008aeb20d9113
Alexander Berglund <albe2003@student.miun.se>
2023-04-11 18:32:30 +0200

    Added functionality for generating Jacoco csv reports

--- downloader/src/main/java/org/miun/Main.java
+++ downloader/src/main/java/org/miun/Main.java
@@ -7,6 +7,16 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import static org.miun.support.Constants.*;
 import java.io.*;
 import java.nio.file.Files;
@@ -17,58 +27,74 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String repoUrl = "https://github.com/apolloconfig/apollo";
         File localRepo = cloneRepo(repoUrl);
-        List<Date> commitDates = getCommitDates(localRepo, 4);
-
-        System.out.println(commitDates.size());
-
-        Date commitDate = commitDates.get(40);
-
-        checkoutCommit(localRepo, commitDate);
-
-        File outputDirectory = new File(OUTPUT_DIRECTORY_PATH);
-        if (!outputDirectory.exists()) {
-            outputDirectory.mkdirs();
-        }
-
-        // Analyze with Designite
-        analyzeWithDesignite(new File(DESIGNITE_JAR_PATH), localRepo, outputDirectory);
-
+//        List<Date> commitDates = getCommitDates(localRepo, 4);
+//
+//        System.out.println(commitDates.size());
+//
+//        Date commitDate = commitDates.get(40);
+//
+//        checkoutCommit(localRepo, commitDate);
+//
+//        File outputDirectory = new File(OUTPUT_DIRECTORY_PATH);
+//        if (!outputDirectory.exists()) {
+//            outputDirectory.mkdirs();
+//        }
+//
+//        // Analyze with Designite
+//        analyzeWithDesignite(new File(DESIGNITE_JAR_PATH), localRepo, outputDirectory);
+//
 //        String architectureSmellsInputFile = new File(outputDirectory, "ArchitectureSmells.csv").getAbsolutePath();
 //        Map<String, Integer> smellCounts = getSmellCounts(architectureSmellsInputFile);
+//
+//        String typeMetricsInputFile = new File(outputDirectory, "TypeMetrics.csv").getAbsolutePath();
+//        double propagationCost = calculatePropagationCost(typeMetricsInputFile);
 
-        String typeMetricsInputFile = new File(outputDirectory, "TypeMetrics.csv").getAbsolutePath();
-        double propagationCost = calculatePropagationCost(typeMetricsInputFile);
-        System.out.println(propagationCost);
-
-//        for (Date commitDate : commitDates) {
-//            // Checkout the commit
-//            checkoutCommit(localRepo, commitDate);
-//
-//            File outputDirectory = new File(OUTPUT_DIRECTORY_PATH);
-//            if (!outputDirectory.exists()) {
-//                outputDirectory.mkdirs();
-//            }
-//
-//            // Analyze with Designite
-//            analyzeWithDesignite(new File(DESIGNITE_JAR_PATH), localRepo, outputDirectory);
-//
-//            // Calculate propagation cost
-//            double propagationCost = calculatePropagationCost(designiteResult);
-//
-//            // Get test coverage data
-//            JacocoResult jacocoResult = getJacocoCoverageData(localRepo);
-//
-//            // Calculate average test coverage
-//            TestCoverageData testCoverageData = calculateTestCoverageData(jacocoResult, designiteResult);
-//
-//            // Write CSV output
-//            writeSnapshotToCSV(commitDate, designiteResult, propagationCost, testCoverageData);
-//        }
+        buildProjectAndGenerateReport(localRepo);
     }
 
+    private static void buildProjectAndGenerateReport(File repoDir) {
+        String command1 = String.format("mvn clean test -DargLine=\"-javaagent:%s=destfile=target/jacoco.exec\"", JACOCO_AGENT_PATH);
+        runCommand(command1, repoDir, false);
+
+        List<File> modules = findModules(repoDir);
+        File customReportsDir = new File(OUTPUT_DIRECTORY_PATH);
+        if (!customReportsDir.exists()) {
+            customReportsDir.mkdirs();
+        }
+
+        for (File module : modules) {
+            if (new File(module, "target/jacoco.exec").exists()) {
+                String moduleName = module.getName();
+                File moduleReportFile = new File(customReportsDir, moduleName + ".csv");
+
+                String reportCommand = String.format("java -jar %s report target/jacoco.exec --classfiles target/classes --sourcefiles src/main/java --csv %s", JACOCO_CLI_PATH, moduleReportFile.getAbsolutePath());
+                runCommand(reportCommand, module, true);
+            }
+        }
+    }
+
+    private static void runCommand(String command, File workingDir, boolean printLines) {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            processBuilder.command(command.split(" "));
+            processBuilder.directory(workingDir);
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("Exited with code: " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     private static File cloneRepo(String repoUrl) {
         try {
@@ -83,6 +109,32 @@ public class Main {
             System.exit(1);
             return null;
         }
+    }
+
+    private static List<File> findModules(File repoDir) {
+        List<File> modules = new ArrayList<>();
+        File pomFile = new File(repoDir, "pom.xml");
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(pomFile);
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("module");
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    String moduleName = element.getTextContent();
+                    File moduleDir = new File(repoDir, moduleName);
+                    modules.add(moduleDir);
+                }
+            }
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+        return modules;
     }
 
     private static List<Date> getCommitDates(File localRepo, int intervalWeeks) {
--- downloader/src/main/java/org/miun/support/Constants.java
+++ downloader/src/main/java/org/miun/support/Constants.java
@@ -3,5 +3,7 @@ package org.miun.support;
 public interface Constants {
 
     String DESIGNITE_JAR_PATH = "/home/alexander/tools/DesigniteJava.jar";
+    String JACOCO_CLI_PATH = "/home/alexander/tools/jacococli.jar";
     String OUTPUT_DIRECTORY_PATH = "/home/alexander/dt133g_project/downloader/src/main/resources";
+    String JACOCO_AGENT_PATH = "/home/alexander/tools/jacocoagent.jar";
 }
