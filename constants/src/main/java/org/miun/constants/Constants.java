edd9c5d709eb9002b71071a13082a0f09dff0e1a
Alexander Berglund <albe2003@student.miun.se>
2023-05-21 13:22:35 +0200

    Slight modifications to config file

--- constants/src/main/java/org/miun/constants/Constants.java
+++ constants/src/main/java/org/miun/constants/Constants.java
@@ -13,6 +13,10 @@ public class Constants {
     public static String DV8_CONSOLE;
     public static String BASE_SNAPSHOT_DIRECTORY;
     public static String RESULTS_DIRECTORY;
+    public static boolean ANALYZE_ARCHITECTURAL_SMELLS;
+    public static boolean ANALYZE_TESTABILITY;
+    public static boolean ANALYZE_MODULARITY;
+    public static Platform PLATFORM;
     public static List<String> OSS_PROJECTS;
 
     static {
@@ -39,6 +43,10 @@ public class Constants {
             BASE_SNAPSHOT_DIRECTORY = config.getProperty("snapshotsDirectory");
             RESULTS_DIRECTORY = config.getProperty("resultsDirectory");
             OSS_PROJECTS = Arrays.asList(config.getProperty("projectUrls").split(","));
+            ANALYZE_ARCHITECTURAL_SMELLS = config.getProperty("analyzeArchitecturalSmells").equalsIgnoreCase("true");
+            ANALYZE_TESTABILITY = !config.getProperty("analyzeTestability").equalsIgnoreCase("false");
+            ANALYZE_MODULARITY = !config.getProperty("analyzeModularity").equalsIgnoreCase("false");
+            PLATFORM = config.getProperty("platform").equalsIgnoreCase("linux") ? Platform.LINUX : Platform.WINDOWS;
         } catch (IOException e) {
             e.printStackTrace();
         }
